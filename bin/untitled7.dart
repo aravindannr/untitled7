6d3b19f40ca745d12acdc698150127b2a53ba885
harikrishnan <kphari891@gmail.com>
2022-11-23 16:01:07 +0530

    Initial commit

---
+++ bin/untitled7.dart
@@ -0,0 +1,13 @@
+void main(){
+  int age = 20;
+  print(age = 20);
+  print(age > 20);
+  print(age < 20);
+  print(age >= 20);
+  print(age <= 20);
+  print(age == 20);
+  print(age != 20);
+
+
+
+}
